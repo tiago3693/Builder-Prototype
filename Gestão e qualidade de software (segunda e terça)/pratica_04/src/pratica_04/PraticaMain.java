6925104730ee1012148907f7c7233e5a917f1275
Tiago de Britto Antunes <tiago_antunes8@hotmail.com>
2022-04-03 21:06:05 -0300

    pratica 04 completa

--- Gestão e qualidade de software (segunda e terça)/pratica_04/src/pratica_04/PraticaMain.java
+++ Gestão e qualidade de software (segunda e terça)/pratica_04/src/pratica_04/PraticaMain.java
@@ -21,6 +21,36 @@ public class PraticaMain {
 		case 3:
 			Pratica_04_3.pratica();
 			break;
+		case 4:
+			Pratica_04_4.pratica();
+			break;
+		case 5:
+			Pratica_04_5.pratica();
+			break;
+		case 6:
+			Pratica_04_6.pratica();
+			break;
+		case 7:
+			Pratica_04_7.pratica();
+			break;
+		case 8:
+			Pratica_04_8.pratica();
+			break;
+		case 9:
+			Pratica_04_9.pratica();
+			break;
+		case 10:
+			Pratica_04_10.pratica();
+			break;
+		case 11:
+			Pratica_04_11.pratica();
+			break;
+		case 12:
+			Pratica_04_12.pratica();
+			break;
+		case 13:
+			Pratica_04_13.pratica();
+			break;
 		}
 
 		scan.close();
---
+++ Gestão e qualidade de software (segunda e terça)/pratica_04/src/pratica_04/Pratica_04_8.java
@@ -0,0 +1,23 @@
+package pratica_04;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Pratica_04_8 {
+
+	public static void pratica() {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Digite codigo do produto: ");
+		String num1 = scan.nextLine();
+
+		String produto[][] = { { "1", "sapato", "99,99" }, { "2", "bolsa", "103,89" }, { "3", "camisa", "49,98" },
+				{ "4", "calça", "89,72" }, { "5", "blusa", " 97,35" } };
+
+		for (String[] i : produto) {
+			if (num1.equals(i[0])) {
+				System.out.print(i[0] + " | " + i[1] + " | " + i[2]);
+			}
+		}
+	}
+}
