4b5984e9055d109ab74ede9ef55d1d37c7c639df
Tiago de Britto Antunes <90283513+tiago3693@users.noreply.github.com>
2022-04-21 22:07:59 -0300

    Add files via upload

---
+++ builder/src/carro/CarroMotor.java
@@ -0,0 +1,13 @@
+package carro;
+
+public class CarroMotor {
+	private final double cilindrada;
+
+	public CarroMotor(double cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+
+	public double getCilindrada() {
+		return cilindrada;
+	}
+}
